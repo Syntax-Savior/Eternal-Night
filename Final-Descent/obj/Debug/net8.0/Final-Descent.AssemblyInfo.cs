041bea83e1ec3131923696eae1bb2dcf5323d452
Simon Michael <simoncodes.m@gmail.com>
2024-12-17 20:37:41 +0300

    Empty Cart Page Completed.

--- Final-Descent/obj/Debug/net8.0/Final-Descent.AssemblyInfo.cs
+++ Final-Descent/obj/Debug/net8.0/Final-Descent.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Final-Descent")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb9286c0001ffab755cd432945c93d2691d3f95e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a575f7085710d257391ec52a538b84138f5b3383")]
 [assembly: System.Reflection.AssemblyProductAttribute("Final-Descent")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Final-Descent")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
